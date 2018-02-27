b2e08604a19861fdcfa873a75d520ad0543e42af
kandascan <boguslaw.boczkowski@gmail.com>
2018-02-27 23:30:18 +0100

    added morgan and mongoose global promise

---
+++ README.md
@@ -0,0 +1,5 @@
+### Need to do
+you will need to create .env file and put your credential for mongodb:
+DB_USER
+DB_PASS
+DB_NAME
--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const port = process.env.PORT || 3000;
 const router = require('./routes/router');
 const config = require('./config/database');
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 app.use('/api', router);
 app.use(express.static(path.join(__dirname, 'public')));
 
+mongoose.Promise = global.Promise;
 mongoose.connect(config.database, function (err) {
     if (err) {
         console.log("Not connected to the database: " + err);

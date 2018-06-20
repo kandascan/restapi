3e059d4202a6a2d64c70546bd6cc01446d82ba6a
Boguslaw Boczkowski <bboczkowski@openskydata.com>
2018-06-20 16:06:02 +0200

    change method

--- server.js
+++ server.js
@@ -73,30 +73,12 @@ io.on('connection', socket => {
             "heatIndexCelsius": data["Heat index"].substr(0, 5),
             "heatIndexFahrenheit": data["Heat index"].substr(9, 5)
             };
-            $.ajax({
-                url: API_URL_MEASURE,
-                type: 'POST',
-                dataType: 'JSON',
-                data: JSON.stringify(measure),
-                contentType: "application/json",
-                success: function (data) {
-                    console.log(data);
-                }
-            });
-        // var measure = {
-        //     "humidity": 12.12,
-        //     "temperatureCelsius": 25.30,
-        //     "temperatureFahrenheit": 77.70,
-        //     "heatIndexCelsius": 25.50,
-        //     "heatIndexFahrenheit": 75.50
-        // };
-        // //console.log(measure);
-        // Measure.addMeasure(measure, function(err, measure){
-        //     if(err){
-        //         throw err;
-        //     }
-        //     res.json(measure);
-        // });
+        Measure.addMeasure(measure, function(err, measure){
+            if(err){
+                throw err;
+            }
+            res.json(measure);
+        });
     });
 
     socket.on('tempSensorServer', data =>{
@@ -114,24 +96,3 @@ io.on('connection', socket => {
     });
 
 });
-
-var insertSensorData = function(data) {
-   
-    var measure = {
-    "humidity": data.Humidity.substr(0, 5),
-    "temperatureCelsius": data.Temperature.substr(0, 5),
-    "temperatureFahrenheit": data.Temperature.substr(9, 5),
-    "heatIndexCelsius": data["Heat index"].substr(0, 5),
-    "heatIndexFahrenheit": data["Heat index"].substr(9, 5)
-    };
-    $.ajax({
-        url: API_URL_MEASURE,
-        type: 'POST',
-        dataType: 'JSON',
-        data: JSON.stringify(measure),
-        contentType: "application/json",
-        success: function (data) {
-            console.log(data);
-        }
-    });
-};

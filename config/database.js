4737925c616160ee5f3cf9b9efed4a03c18d2823
Boguslaw Boczkowski <bboczkowski@openskydata.com>
2018-06-21 16:38:53 +0200

    update database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 require('dotenv/config')
 module.exports = {
     database: process.env.CONNECTION_STRING  
-   // database: 'mongodb://task:task@ds251827.mlab.com:51827/task'  
+   // database: 'mongodb://test:test@ds153400.mlab.com:53400/testowa' 
 }
